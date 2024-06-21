53476c85efa1786b16153b5b5ff41cd06cd83155
David Sommer <sommer@wias-berlin.de>
2024-06-21 10:30:16 +0200

    added Random.h from learncpp.com for random number generation and added a normal random number generator

--- main.cpp
+++ main.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
 #include "options.h"
 #include "compounding.h"
+#include "Random.h"
+#include <iostream>
 
 auto main() -> int
 {
 	//Options::callCreditSpreadUnitTest();
 
-	Compounding::discreteUnitTest();
+	//Compounding::discreteUnitTest();
+
+	for (int k{1}; k <= 10; k++)
+	{
+		std::cout << Random::normal(0.0, 1.0) << "\n";
+	}
 
 	return 0;
 }

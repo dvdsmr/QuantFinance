14f9b000a0b2571633cf3ce0f652c121c0cfa28f
David <sommer@wias-berlin.de>
2024-08-13 11:54:22 +0200

    addded simulation (euler-Maruyama) of OU process

--- sdes.cpp
+++ sdes.cpp
@@ -9,6 +9,18 @@ namespace SDE
 	{
 		return state + stepSize * drift * state + std::sqrt(stepSize) * diffusion * Random::normal(0.0,1.0);
 	}
+	auto OrnsteinUhlenbeckSimulate(double state, double stepSize, double drift, double diffusion, double time) -> double
+	{
+		double terminalState = state;
+		double currentTime = 0.0;
+		while (currentTime <= time-stepSize)
+		{
+			terminalState = OrnsteinUhlenbeckStep(terminalState,stepSize,drift,diffusion);
+			currentTime += stepSize;
+		}
+		terminalState = OrnsteinUhlenbeckStep(terminalState, time-currentTime, drift, diffusion);
+		return terminalState;
+	}
 	auto geometricBrownianMotion(double initialState, double time, double drift, double volatility) -> double
 	{
 		return initialState * std::exp((drift - std::pow(volatility, 2) / 2) * time + volatility * std::sqrt(time) * Random::normal(0.0, 1.0));
--- sdes.h
+++ sdes.h
@@ -5,6 +5,7 @@ namespace SDE
 {
 	auto OrnsteinUhlenbeckStep(double state, double stepSize, double drift, double diffusion) -> double;
 	auto geometricBrownianMotion(double initialState, double time, double drift, double volatility) -> double;
+	auto OrnsteinUhlenbeckSimulate(double state, double stepSize, double drift, double diffusion, double time) -> double;
 }
 
 #endif

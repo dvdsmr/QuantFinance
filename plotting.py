85db1ccb8f6e1d117352f7ff3d553b489848f79d
David <sommer@wias-berlin.de>
2024-08-15 18:55:27 +0200

    added plotting with Python script

--- plotting.py
+++ plotting.py
@@ -1,4 +1,7 @@
 from matplotlib import pyplot as plt
+import csv
+from numpy import genfromtxt
+
 
 def plotStockPath(stockVals, timeVals):
     assert len(stockVals) == len(timeVals)
@@ -8,6 +11,18 @@ def plotStockPath(stockVals, timeVals):
     ax.set_ylabel("Stock price")
     plt.savefig("plots/stockPath.png")
     plt.plot()
+
+def plotStockCSV(filename):
+    stockData = genfromtxt(filename, delimiter=',')
+    fig, ax = plt.subplots(figsize=(10, 5))
+    ax.plot(stockData[:,0],stockData[:,1],'b-')
+    ax.set_xlabel("Time")
+    ax.set_ylabel("Stock price")
+    plt.savefig("plots/stockPath.png")
+    plt.plot()
+
+if __name__ == "__main__":
+    my_data = plotStockCSV("Data/stockPath")
 
 
 

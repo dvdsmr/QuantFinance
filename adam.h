33f71a502ae76b5565fa11e90e5e5ce9ef040948
David <sommer@wias-berlin.de>
2024-09-15 21:55:05 +0200

    included volSurface calibration and fixed adam linkage error

--- adam.h
+++ adam.h
@@ -1,7 +1,7 @@
 #ifndef ADAM_H
 #define ADAM_H
 
-
+#include <iostream>
 
 class Adam
 {
@@ -32,6 +32,9 @@ public:
         {}
     };
 
+    // setter
+    void set_state(double initialVal) { m_state = { initialVal,0.0 }; }
+
     // updates the weight and bias of Adam in place
     constexpr void update(const auto& deriv);
     constexpr double optimize(const auto& func, const auto& deriv, bool verbose=false);
@@ -48,6 +51,55 @@ private:
     weightAndBias m_variances{};
 };
 
+
+// based on a medium article: https://medium.com/the-ml-practitioner/how-to-implement-an-adam-optimizer-from-scratch-76e7b217f1cc
+constexpr void Adam::update(const auto& deriv)
+{
+    weightAndBias gradWeightBias{ deriv(m_state.m_weight), deriv(m_state.m_bias) };
+    // momentum beta
+    // *** weights *** 
+    m_means.m_weight = m_firstOrderExpDecay * m_means.m_weight + (1 - m_firstOrderExpDecay) * gradWeightBias.m_weight;
+    // *** biases *** 
+    m_means.m_bias = m_firstOrderExpDecay * m_means.m_bias + (1 - m_firstOrderExpDecay) * gradWeightBias.m_bias;
+
+    // rms beta
+    // *** weights *** 
+    m_variances.m_weight = m_secondOrderExpDecay * m_variances.m_weight + (1 - m_secondOrderExpDecay) * (gradWeightBias.m_weight * gradWeightBias.m_weight);
+    // *** biases *** 
+    m_variances.m_bias = m_secondOrderExpDecay * m_variances.m_bias + (1 - m_secondOrderExpDecay) * (gradWeightBias.m_bias);
+
+    // bias correction
+    weightAndBias meanCorrection{ m_means.m_weight / (1 - m_firstOrderExpDecay), m_means.m_bias / (1 - m_firstOrderExpDecay) };
+    weightAndBias varianceCorrection{ m_variances.m_weight / (1 - m_secondOrderExpDecay), m_variances.m_bias / (1 - m_secondOrderExpDecay) };
+
+    // update weights and biases
+    m_state.m_weight = m_state.m_weight - m_stepSize * (meanCorrection.m_weight / (std::sqrt(varianceCorrection.m_weight) + m_tol));
+    m_state.m_bias = m_state.m_bias - m_stepSize * (meanCorrection.m_bias / (std::sqrt(varianceCorrection.m_bias) + m_tol));
+
+    return;
+}
+
+
+constexpr double Adam::optimize(const auto& func, const auto& deriv, bool verbose)
+{
+    bool converged{ false };
+    int numIt{ 0 };
+    double oldWeight{};
+    while (!converged)
+    {
+        oldWeight = m_state.m_weight;
+        update(deriv);
+        converged = (std::abs(m_state.m_weight - oldWeight) < m_tol);
+        ++numIt;
+    }
+    if (verbose)
+    {
+        std::cout << "Adam optimization converged after " << numIt << " gradient steps.\n";
+        std::cout << "Parameter value is " << m_state.m_weight << "\n";
+        std::cout << "with a function value of " << func(m_state.m_weight) << ".\n";
+    }
+    return m_state.m_weight;
+}
 
 void testAdam();
 
--- volatility.cpp
+++ volatility.cpp
@@ -1,6 +1,7 @@
 #include "numpy.h"
 #include "xyvals.h"
 #include "options.h"
+#include "adam.h"
 #include <string>
 #include <string_view>
 
@@ -24,22 +25,46 @@ namespace Volatility
 			);
 
 			// define strikes and maturities
-			volSurface.m_rowVals = priceSurface.m_rowVals;
-			volSurface.m_colVals = priceSurface.m_rowVals;
+			volSurface.m_rowVals = priceSurface.m_rowVals; // time to maturity
+			volSurface.m_colVals = priceSurface.m_rowVals; // strike
 
-			// get the table values by the BSM model
+			// calibrate the vol surface. 
+			double volGuess{ 0.1 };
+			Adam adam{}; 
 			for (std::size_t row{ 0 }; row < std::size(volSurface.m_rowVals); ++row)
 			{
+				adam.set_state(volGuess); // initialize Adam with a guess of the volatility
 				for (std::size_t col{ 0 }; col < std::size(volSurface.m_colVals); ++col)
 				{
-					double vol{ 0.1 };
-					// ToDO: here we need to minimize the right hand side over the vol parameter
-					volSurface.m_table[row][col] = Options::Pricing::BSM::call(riskFreeReturn,
-						vol,
-						priceSurface.m_rowVals[row],
-						priceSurface.m_colVals[col],
-						spot,
-						dividendYield) - priceSurface.m_table[row][col];
+					double truePrice{ priceSurface.m_table[row][col] };
+
+					// define adam target function and derivative
+					auto func
+					{
+						[&](double vol) {
+							double price{ Options::Pricing::BSM::call(riskFreeReturn, vol, priceSurface.m_rowVals[row], priceSurface.m_colVals[col], spot, dividendYield) };
+							return (price - truePrice) * (price - truePrice);
+						}
+					};
+					auto deriv
+					{
+						[&](double vol) {
+							double price{ Options::Pricing::BSM::call(riskFreeReturn, vol, priceSurface.m_rowVals[row], priceSurface.m_colVals[col], spot, dividendYield) };
+							return 2 * (price - truePrice) * Options::Pricing::BSM::callVega(riskFreeReturn, vol, priceSurface.m_rowVals[row], priceSurface.m_colVals[col], spot, dividendYield);
+						}
+					};
+
+					// Note that adam keeps the optimized vol as its state, so each iteration
+					// starts with the previously calibrated vol
+					volSurface.m_table[row][col] = adam.optimize(func, deriv);
+
+					// once we have iterated through all strikes for one maturity, we want
+					// to start the optimizer with the vol previously calibrated for the lowest strike
+					if (col == static_cast<std::size_t>(0))
+					{
+						volGuess = volSurface.m_table[row][col];
+					}
+
 				}
 			}
 
